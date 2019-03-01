4486037a334c12998c3c2b47a387a8ba25bbd26d
nmajstorovic <nemanja.majstorovic@ubisoft.com>
2019-03-01 15:05:23 +0100

    Update gitignore and revert problem 14

--- LargeSum/LargeSum/Program.cs
+++ LargeSum/LargeSum/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Numerics;
-using System.Diagnostics;
-
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LargeSum
 {
@@ -12,18 +12,48 @@ namespace LargeSum
         static void Main(string[] args)
         {
             string filename = Environment.CurrentDirectory + "\\numbers.txt";
-            BigInteger result = new BigInteger();
+            List<string> numbers = readInput(filename);
+            int digits = 0;
+            for (int i = 11; i == 0; i--)
+            {
+                int sum = 0;
+                foreach ( string number in numbers)
+                {
+                    sum = sum + System.Convert.ToInt32(number[i]);
+                }
+                digits = sum;
+            }
+
+
+            Console.WriteLine(digits);
+            Console.ReadLine();
+        }
+
+
+        private static List<string> readInput(string filename)
+        {
             string line;
+            string[] linePieces;
+
             StreamReader r = new StreamReader(filename);
 
+            List<string> inputNumbers = new List<string>();
+            r.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            int j = 0;
             while ((line = r.ReadLine()) != null)
             {
-                result += BigInteger.Parse(line);
+                linePieces = line.Split(' ');
+                for (int i = 0; i < linePieces.Length; i++)
+                {
+                    inputNumbers.Add(linePieces[i]);
+                }
+                j++;
             }
 
             r.Close();
-            Console.WriteLine(result.ToString().Substring(0, 10));
-            Console.ReadLine();
+
+            return inputNumbers;
         }
     }
 }
